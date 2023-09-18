3107e103dbc18708ec576fcdce6bcb04a126bc90
MandJ2022 <96705392+MandJ2022@users.noreply.github.com>
2023-09-18 19:05:48 +0200

    Add files via upload

---
+++ mygov.js
@@ -0,0 +1,19 @@
+const mygov = document.querySelectorAll('#mygov path');
+
+for(let i = 0; i<mygov.length; i++){
+    console.log(`Letter ${i} is ${mygov[i].getTotalLength()}`);
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add(`show`);
+        } else {
+            entry.target.classList.remove(`show`);
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(`.flexcontainer`);
+hiddenElements.forEach((el) => observer.observe(el));
